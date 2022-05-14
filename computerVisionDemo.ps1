4bad26e8e676e20f11b9e540ff3102bed7a246f9
vlradu <vlad13radu@outlook.com>
2022-05-14 16:44:13 +0300

    Initial commit

--- computerVisionDemo.ps1
+++ computerVisionDemo.ps1
@@ -1,6 +1,25 @@
-#Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+# MIT License
 
-#Using default Photos app won't return Process Id
+# Copyright (c) 2022 Vlad-Andrei Radu
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 <#
 .SYNOPSIS
 Demo for Computer Vision APIs.
@@ -45,19 +64,6 @@ $PhotoAppPath = "C:\Program Files\IrfanView\i_view64.exe"
 #endregion
 
 #region FunctionDefinitions
-
-<#function Convert-ImageToBase64{
-    Param([String] $Path)
-    $ImageB64 = [Convert]::ToBase64String((Get-Content $Path -AsByteStream))
-    return $ImageB64
-}
-
-function ConvertTo-Raw{
-    Param([String] $Path) 
-    $RawImage = Get-Content $Path -AsByteStream -Raw
-    #[byte []]$RawImage = [System.IO.File]::ReadAllBytes($Path)
-    return $RawImage
-}#>
 
 function Send-PostRequest{
     param(
